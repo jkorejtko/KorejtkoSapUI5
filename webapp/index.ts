91f50b63064031579d82eda347046c1953fcfab6
Jiri Korejtko <jiri.korejtko@gmail.com>
2025-05-18 21:37:22 +0200

    feat: walkthrough - page, panel, i18n

--- webapp/index.ts
+++
@@ -1,8 +0,0 @@
-import XMLView from "sap/ui/core/mvc/XMLView";
-
-XMLView.create({
-    viewName: "ui5.walkthrough.view.App",
-    id: "app"
-}).then(function (view) {
-    view.placeAt("content");
-});
