cf42257bff8e5dcf1fbb8bae962df7965f3b5da2
Jiri Korejtko <jiri.korejtko@gmail.com>
2025-05-18 22:09:06 +0200

    feat: customHeader, localization for EN and CS langs

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,8 +1,25 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 /**
- * @namespace ui5.walkthrough.controller
+ * @name ui5.walkthrough.controller.App
  */
-export default class App extends Controller {
+export default class AppController extends Controller {
+    /**
+     * Event handler for language change
+     * @param {sap.ui.base.Event} oEvent - Event object
+     */
+    onLanguageChange(oEvent: sap.ui.base.Event): void {
+      // Získání zvoleného jazyka z Selectu
+      const sSelectedLang = oEvent.getSource().getSelectedKey();
 
+      // Vytvoření nového modelu s lokalizací
+      const oResourceModel = new ResourceModel({
+        bundleName: "ui5.walkthrough.i18n.i18n",
+        bundleLocale: sSelectedLang
+      });
+
+      // Nastavení nového modelu
+      this.getView()?.setModel(oResourceModel, "i18n");
+    }
 };
